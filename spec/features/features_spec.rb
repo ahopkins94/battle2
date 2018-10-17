c9fb9088f96cd40f3a675d145fce9e3d8819016a
Rupert Adams <RupertNCAdams@aol.com>
2018-10-17 15:16:41 +0100

    making a play.erb file

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,16 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+# feature 'Testing infrastructure' do
+#   scenario 'Can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+
+feature 'Testing battle' do
+  scenario 'can submit names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in('player_1',with: "Steve")
+    fill_in('player_2',with: "Bob")
+    click_button('Submit')
+    expect(page).to have_content "Steve vs Bob"
   end
 end
